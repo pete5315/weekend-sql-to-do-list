24589627cb23e281fd1ccf83c81c183fda02e741
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-05-18 21:28:27 -0500

    GET and POST functions working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -42,11 +42,47 @@ function getRequest() {
 function renderToDOM(tasks) {
     $('#taskTable').empty();
     for (let task of tasks) {
+        $('#taskTable').append(`
         <tr>
             <td id="titleData">${task.description}</td>
-            <td id="authorData">${book.author}</td>
+            <td id="authorData">${task.author}</td>
             <td><button class="edit" onclick="updateMode(this)" data-value="${task.id}" title-name="${task.description}" author-name="${task.author}" >Edit!</button></td>
             <td><button class="delete" onclick="deleteBooks(this)" data-value="${task.id}">Delete!</button></td>
         </tr>
+        `)
     }
 }
+
+// function putRequest(){
+//     //get id to update
+//     $.ajax({
+//         method: 'PUT',
+//         url: `/books/${taskId}`,
+//         // data: {
+//         //     isComplete: 'True'
+//         // }
+//     })
+//     .then(function(response){
+//         getRequest();
+//     })
+//     .catch( function(error) {
+//         alert('Error:', error);
+//     })
+// }
+
+// function deleteRequest(){
+//     //get id to update
+//     $.ajax({
+//         method: 'DELETE',
+//         url: `/books/${taskId}`,
+//         // data: {
+//         //     isComplete: 'True'
+//         // }
+//     })
+//     .then(function(response){
+//         getRequest();
+//     })
+//     .catch( function(error) {
+//         alert('Error:', error);
+//     })
+// }
