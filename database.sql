b5b0a00d8ac11dfcd23daf23e4fe237053149648
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-05-22 09:37:30 -0500

    missed the pool file for updating databse name, that is changed now

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
 --table creation code for Postico
-CREATE TABLE "tasks" (
+CREATE TABLE "weekend-to-do-app" (
 	"id" SERIAL PRIMARY KEY,
 	"description" VARCHAR (400) NOT NULL,
 	"author" VARCHAR (100) NOT NULL,
     "isComplete" BOOLEAN DEFAULT FALSE
 );
 
-ALTER TABLE "tasks" ADD COLUMN "timeCompleted" DATE;
+ALTER TABLE "weekend-to-do-app" ADD COLUMN "timeCompleted" DATE;
